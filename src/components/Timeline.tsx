8e9521f6d3bb82e09846f9737122e472ec581d00
Toggytokyo <anthony.l.digiovanna@gmail.com>
2026-03-01 13:09:58 -0500

    Created basic controls for timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
+//Represents the data for the draggable audio blocks
+interface TimelineClip {
+  id: string;
+  name: string;
+  duration: number;
+  startTime: number;
+}
+
 export default function Timeline() {
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="timeline">
-      <h3>Timeline</h3>
+      <div className="header">
+        <div className="controls">
+          <button
+            title="Return to start"
+          >⏮</button>
+          <button
+            onClick={() => setIsPlaying(p => !p)}
+          >
+            {isPlaying ? '⏸' : '▶'}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
